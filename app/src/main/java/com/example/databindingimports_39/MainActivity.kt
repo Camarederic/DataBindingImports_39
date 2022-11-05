53e8f73a6e0710083246d8fa90e21c9271d13828
Camarederic <camarederic@mail.ru>
2022-11-05 16:38:48 +0300

    Create DataBinding Imports

--- app/src/main/java/com/example/databindingimports_39/MainActivity.kt
+++ app/src/main/java/com/example/databindingimports_39/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.databindingimports_39
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.databindingimports_39.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val myUser = User("Ruslan", "Ibragimov", 39, true)
+
+        binding.user = myUser
     }
 }
